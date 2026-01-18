76fda4e2619a89ccd3d6768e00a666aad34f6592
wulala0102 <sarahwu8788@gmail.com>
2026-01-18 16:00:27 +0800

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,18 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+const rootPath = path.resolve(__dirname, 'demo');
+const outDirPath = path.resolve(__dirname, 'dist');
+
+console.log('__dirname:', __dirname);
+console.log('root path:', rootPath);
+console.log('outDir path:', outDirPath);
+
 export default defineConfig({
   plugins: [react()],
-  root: 'demo',
+  root: rootPath,
   build: {
-    outDir: '../dist',
+    outDir: outDirPath,
     emptyOutDir: true,
   },
   server: {
